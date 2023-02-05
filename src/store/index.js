9924a6e6e7ae74048c55b8194c46258246e767e2
Babashov Mahammad <babashov.info@gmail.com>
2023-02-05 17:08:40 +0400

    init

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import {createStore} from 'redux'
+
+const counterReducer = (state = {counter:0},payload)=>{
+
+    if(payload.type === 'increment')
+    {
+        return {
+            counter: state.counter + 1
+        }
+    }
+
+    if(payload.type === 'decrement')
+    {
+        return {
+            counter:state.counter - 1
+        }
+    }
+
+    return state;
+}
+
+const store = createStore(counterReducer)
+
+
+export default store;
